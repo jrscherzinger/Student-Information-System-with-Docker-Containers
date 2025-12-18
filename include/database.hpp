ff506bf941653df8023d205f09c0ba5318e58041
tahaozdemirr <taha.ozdemir@stu.fbu.edu.tr>
2025-12-18 19:02:07 +0300

    chore: create initial C++ project

---
+++ include/database.hpp
@@ -0,0 +1,7 @@
+
+#ifndef DATABASE_HPP
+#define DATABASE_HPP 
+
+// database connection and operations will be implemented later
+
+#endif // DATABASE_HPP
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "student information system" << endl;
+	return 0;
+
+}
